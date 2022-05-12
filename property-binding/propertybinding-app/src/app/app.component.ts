cc31a085657a68f5a8a5af3c10a4894cfb318425
nidhi-softvan <nidhijain.softvan@gmail.com>
2022-05-12 17:47:49 +0530

    property-binding

---
+++ property-binding/propertybinding-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'propertybinding-app';
+  name="nidhi";
+  disable=false;
+  show=true;
+  color="blue"
+}
